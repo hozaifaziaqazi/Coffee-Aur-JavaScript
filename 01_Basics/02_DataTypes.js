26dcc4dfcf9fa7fb2a7ecd0e1e70af8b76df2450
Hozaifa Zia <hozaifaziaqazi@gmail.com>
2024-05-09 21:14:02 +0500

    02_DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,39 @@
+console.log("Topic: Data Types");
+
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+/* console.log(3 
++
+ 3); console.log("Hozaifa") */ // High Your Code Readibility
+
+// write like this
+console.log(3 + 3)
+console.log("Hozaifa")
+
+
+let id = undefined
+let fname = "hozaifa"
+let age = 15
+let isLoggeIn  = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+// "typeof" means type of Variable or given Data
+
+console.log(typeof id)
+console.log(typeof fname)
+console.log(typeof age)
+console.log(typeof isLoggeIn)
+console.log(typeof state)
