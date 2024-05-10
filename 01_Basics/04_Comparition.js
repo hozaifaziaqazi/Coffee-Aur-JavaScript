7b9b7f8e97a14af1828e187a4efce5e8a176714b
Hozaifa Zia <hozaifaziaqazi@gmail.com>
2024-05-10 19:27:29 +0500

    04_Comparition

---
+++ 01_Basics/04_Comparition.js
@@ -0,0 +1,27 @@
+console.log("Comparition Of Datatypes");
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// use only same datatypes because difrent datatype can creat problems
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+// console.log("Pause");
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+
+// link to learn datatypes w3school.com
