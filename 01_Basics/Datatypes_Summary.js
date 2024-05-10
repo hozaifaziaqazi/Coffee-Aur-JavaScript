4e3a6bbaee674a66948c206fb82a931d76107a30
Hozaifa <hozaifaziaqazi@gmail.com>
2024-05-10 23:04:04 +0500

    Datatypes_Summary

---
+++ 01_Basics/Datatypes_Summary.js
@@ -0,0 +1,58 @@
+console.log("Summary Of Datatypes");
+
+// There Are Two types of Datatypes:
+// Premitive
+// Non-Premitive
+
+
+
+/* <---------- Premitive-Start ----------> */
+
+// Premitive
+// 7 Types in Premitive
+// 1. String | " "
+// 2. Number | 231
+// 3. Boolearn | true or false
+// 4. Null | 0 or noting
+// 5. Undefined | means not defiend
+// 6. Symbol | to make anything unique
+// 7. BigInt | to manuplate big numbers
+
+// Examples:
+// const a = 100;  console.log(typeof a);
+// const b = 100.5;  console.log(typeof b); 
+// const c = 'abc';  console.log(typeof c);
+// const d = true;  console.log(typeof c);
+// const e = false;  console.log(typeof e);
+// const f = null;  console.log(typeof f);
+// const g = undefined;  console.log(typeof g);  
+// const h = Symbol('123');  console.log(typeof h); console.log(g);
+// const i = 100n;  console.log(typeof i);
+
+// const j = Symbol('678');
+// const k = Symbol('678');
+// console.log(j === k);
+
+// const l = 1234567891011121314151617181920n; // writ 'n' inthe end of number to change it to bigint
+
+/* <----------- Premitive-End -----------> */ 
+
+
+
+/* <-------- Non-Premitive-Start --------> */
+
+// Refrence (Non-Premitive)
+// 3 Types in Non-Premitive
+// 1. Array | similer to python list
+// 2. Object | similer to python dict
+// 3. Function | similer to pythons def function
+
+// Examples:
+// let heros = ["ironman", "thor", "hulk", "spiderman"];
+// let hero = { name: "ironman", age: 45 }
+//let myfunc = function() {console.log("Hello My Function");}
+
+//console.log(typeof heros); // output will be 'object' 
+//console.log(typeof (hero)); // output will be 'object'
+//console.log(typeof myfunc); // output will be 'function' but real is 'object function' 
+/* <--------- Non-Premitive-End ---------> */
