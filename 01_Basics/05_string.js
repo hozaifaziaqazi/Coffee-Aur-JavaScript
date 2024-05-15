a138d234812d28895169383b0d09ef7aa9b6b171
Hozaifa <hozaifaziaqazi@gmail.com>
2024-05-15 19:58:27 +0500

    added strings.js file

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,90 @@
+console.log("Strings In JS");
+
+// const myname = "Hozaifa Zia"
+// const repo_count = 15
+// console.log("Old Way↓");
+// console.log("Hello My name is " + myname + " My Repositry Count is " + repo_count);
+// console.log("New Way↓");
+// console.log(`Hello My Name is ${myname} and My Repositry Count is ${repo_count}`);
+
+// this will be typeof object not string
+const User_Name = new String(" pneumonoultramicroscopicsilicovolcanoconiosis ")
+// console.log(User_Name);
+// console.log(typeof User_Name);
+// console.log(User_Name.toLowerCase());
+// console.log(User_Name[0]);
+// console.log(User_Name.__proto__); // you have not to use .__proto__ before any function
+// console.log(User_Name.length);
+// console.log(User_Name.toUpperCase());
+// console.log(User_Name.charAt(21));
+// console.log(User_Name.indexOf("p"));
+// console.log(User_Name);
+
+
+// all string functions is here 
+
+// const Second_User = User_Name.substring(0, 5)
+// console.log(Second_User);
+
+// const Third_User = User_Name.slice(0, 5)
+// console.log(Third_User);
+
+// const Fourth_User = User_Name.substr(0, 5)
+// console.log(Fourth_User);
+
+// const Fifth_User = User_Name.split("p")
+// console.log(Fifth_User);
+
+// const Sixth_User = User_Name.concat(" hozaifa")
+// console.log(Sixth_User);
+
+// const Seventh_User = User_Name.replace("p", "h") // here it will only change first p letter
+// console.log(Seventh_User)
+
+// const Eighth_User = User_Name.replaceAll("p", "z") // here it will change all p letter
+// console.log(Eighth_User)
+
+// const Ninth_User = User_Name.trim()
+// console.log(Ninth_User)
+
+// const Tenth_User = User_Name.valueOf()
+// console.log(Tenth_User)
+
+// const Eleventh_User = User_Name.toString()
+// console.log(Eleventh_User)
+
+// const Twelveth_User = User_Name.toLocaleLowerCase('en-US')
+// console.log(Twelveth_User)
+
+// const Thirteenth_User = User_Name.toLocaleUpperCase('en-US')
+// console.log(Thirteenth_User)
+
+// const Fourteenth_User = User_Name.toLowerCase()
+// console.log(Fourteenth_User)
+
+// const Fifteenth_User = User_Name.toUpperCase()
+// console.log(Fifteenth_User)
+
+// const Sixteenth_User = User_Name.includes("pneumonoul",21)
+// console.log(Sixteenth_User)
+
+// const Seventeenth_User = User_Name.startsWith("n") // string did not start with p its start with space mean " "
+// console.log(Seventeenth_User)
+
+// const Eighteenth_User = User_Name.endsWith("s")
+// console.log(Eighteenth_User)
+
+// const Nineteenth_User = User_Name.repeat(8)
+// console.log(Nineteenth_User)
+
+// const Twentieth_User = User_Name.padStart(48, "Hi")
+// console.log(Twentieth_User)
+
+// const Twentyfirst_User = User_Name.padEnd(49, "Bye")
+// console.log(Twentyfirst_User)
+
+// const Twentysecond_User = User_Name.trimStart()
+// console.log(Twentysecond_User)
+
+// const Twentythird_User = User_Name.trimEnd()
+// console.log(Twentythird_User)
