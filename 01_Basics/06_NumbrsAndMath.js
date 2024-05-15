f867588703a579bad4c1b70e2733383e711db1a6
Hozaifa <hozaifaziaqazi@gmail.com>
2024-05-15 23:50:00 +0500

    added 06_NumbrsAndMath file and added the (Numbers)

---
+++ 01_Basics/06_NumbrsAndMath.js
@@ -0,0 +1,32 @@
+console.log("Numbers And Math In JS");
+
+/* *************** Numbers *************** */     
+
+console.log("Numbers In JS");
+/* <----------- Numbers Start -----------> */     
+
+const User_Balance = new Number(100)
+const User_Price = new Number(143.573)
+const User_Hundredes = new Number(10000000000)
+// console.log(User_Balance);
+// console.log(typeof (User_Balance));
+
+// all Numbers (Objects) functions is here 
+
+// const User_First = User_Balance.toString()
+// console.log(User_First);
+// // now i can use the strings functions
+// console.log(User_First.length);
+// console.log(typeof (User_First));
+
+// const User_Second = User_Price.toFixed(2)
+// console.log(User_Second);
+
+// const User_Third = User_Price.toPrecision(4)
+// console.log(User_Third);
+
+// const User_Fourth = User_Hundredes.toLocaleString();
+// console.log(User_Fourth);
+
+/* <----------- Numbers End ------------> */
+
