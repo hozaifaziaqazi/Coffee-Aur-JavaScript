9bfa11fa673f16a66535ad392a2dff8b31ab8bcd
Hozaifa <hozaifaziaqazi@gmail.com>
2024-05-17 01:29:09 +0500

    added math in 06_NumbrsAndMath.js file

--- 01_Basics/06_NumbrsAndMath.js
+++ 01_Basics/06_NumbrsAndMath.js
@@ -1,21 +1,20 @@
 console.log("Numbers And Math In JS");
 
-/* *************** Numbers *************** */     
+/* ++++++++++++++++++++ Numbers ++++++++++++++++++++ */
 
-console.log("Numbers In JS");
-/* <----------- Numbers Start -----------> */     
+// console.log("Numbers In JS");
+/* <---------------- Numbers Start ----------------> */
 
-const User_Balance = new Number(100)
-const User_Price = new Number(143.573)
-const User_Hundredes = new Number(10000000000)
+// const User_Balance = new Number(100)
+// const User_Price = new Number(143.573)
+// const User_Hundredes = new Number(10000000000)
 // console.log(User_Balance);
 // console.log(typeof (User_Balance));
 
 // all Numbers (Objects) functions is here 
 
-// const User_First = User_Balance.toString()
+// const User_First = User_Balance.toString() // now i can use the strings functions
 // console.log(User_First);
-// // now i can use the strings functions
 // console.log(User_First.length);
 // console.log(typeof (User_First));
 
@@ -28,5 +27,87 @@ const User_Hundredes = new Number(10000000000)
 // const User_Fourth = User_Hundredes.toLocaleString();
 // console.log(User_Fourth);
 
-/* <----------- Numbers End ------------> */
+/* <---------------- Numbers End -----------------> */
 
+
+
+
+
+/* +++++++++++++++++++++ Math +++++++++++++++++++++ */
+
+console.log("Maths In JS");
+/* <----------------- Math Start -----------------> */
+
+// const User_Math = Math()
+const User_NegitiveNumber = -7;
+const User_DecimalOne = 4.2;
+const User_DecimalTwo = 4.8;
+const User_MutipleNumbers = (95, 3, 68);
+
+
+// all Maths (Objects) functions is here 
+
+// const User_First = Math.abs(User_NegitiveNumber) // it will only convet negative numbers to positive
+// console.log(User_First);
+
+// const User_Second = Math.round(User_DecimalOne)
+// console.log(User_Second);
+
+// const User_Third = Math.ceil(User_DecimalOne) // it will round up to the top value
+// console.log(User_Third);
+
+// const User_Fourth = Math.floor(User_DecimalTwo) // it will round down to the bottom value
+// console.log(User_Fourth);
+
+// const User_Fifth = Math.min(95, 29, 3, 68)
+// console.log(User_Fifth);
+
+// const User_Sixth = Math.max(97, 2, 5, 37)
+// console.log(User_Sixth); 
+
+// const User_Seventh = Math.pow(5, 5) // 5 to the power of 4
+// console.log(User_Seventh);
+
+// const User_Eighth = Math.sqrt(25) // square root of 25
+// console.log(User_Eighth);
+
+// const User_Tenth = Math.random() // it will alway print between 0 and 1
+// console.log(User_Tenth);
+
+
+// const User_Eleventh = Math.trunc(4.2) // Remove all decimal part of number.
+// console.log(User_Eleventh) // 4
+
+// const User_Twelfth = Math.log(5) // Returns natural logarithm of given number.
+// console.log(User_Twelfth) // 1.6094379124341003
+
+// const User_Thirteenth = Math.log10(5) // Returns base 10 logarithm of given number.
+// console.log(User_Thirteenth) // 0.6989700043360187
+
+// const User_Fourteenth = Math.log2(5) // Returns base 2 logarithm of given number.
+// console.log(User_Fourteenth) // 2.321928094887362
+
+// const User_Fifteenth = Math.exp(5) // Returns the value of e^x.
+// console.log(User_Fifteenth) // 148.4131591025766
+
+// const User_Sixteenth = Math.cbrt(5) // Returns cube root of given number.
+// console.log(User_Sixteenth) // 1.4422495703074083
+
+// const User_Seventeenth = Math.hypot(3, 4) // Returns square root of sum of squares of arguments.
+// console.log(User_Seventeenth) // 5
+ 
+const User_Eighteenth = Math.clz32(5) // Returns number of leading zero bits in 32-bit binary
+// representation of number.
+console.log(User_Eighteenth) // 29
+
+
+// Random Formula
+
+// const min = 10;
+// const max = 20;
+// console.log((Math.random()*10) + 1);
+// console.log(Math.round(Math.random()*10) + 1);
+// console.log(Math.round(Math.random() * (max -min + 1)) + min);
+
+
+/* <------------------ Math End ------------------> */
