af228c3104fb46f865b37656fe1775202343fd97
Hozaifa <hozaifaziaqazi@gmail.com>
2024-05-17 10:08:50 +0500

    added 07_DateAndTime file

---
+++ 01_Basics/07_DateAndTime.js
@@ -0,0 +1,58 @@
+console.log("Dates And Time In Js");
+
+
+/* +++++++++++++++++++++ Dates +++++++++++++++++++++ */
+
+console.log("Dates In Js");
+
+/* <----------------- Dates Start -----------------> */
+
+const MyDate = new Date();
+
+// const User_First = MyDate.getTime();
+// console.log('getTime:', User_First);
+
+// const User_Second = MyDate.toDateString();
+// console.log('toDateString:', User_Second);
+
+// const User_Third = MyDate.toISOString();
+// console.log('toISOString:', User_Third);
+
+// const User_Fourth = MyDate.toJSON();
+// console.log('toJSON:', User_Fourth);
+
+// const User_Fifth = MyDate.toLocaleDateString();
+// console.log('toLocaleDateString:', User_Fifth);
+
+// const User_Sixth = MyDate.toLocaleString();
+// console.log('toLocaleString:', User_Sixth);
+
+// const User_Seventh = MyDate.toLocaleTimeString();
+// console.log('toLocaleTimeString:', User_Seventh);
+
+// const User_Eighth = MyDate.toString();
+// console.log('toString:', User_Eighth);
+
+
+// Custom Dates
+// const MyCreatedDate = new Date(2009, 7, 17, 9, 30, 0);
+// const MyCreatedDate = new Date("2009-8-17");
+const MyCreatedDate = new Date("1-08-2009");
+// console.log(MyCreatedDate.toString());
+// console.log(MyCreatedDate.toDateString());
+// console.log(MyCreatedDate.toTimeString());
+// console.log(MyCreatedDate.toLocaleString());
+// console.log(MyCreatedDate.getTime());
+
+const MyTimeStamp = Date.now();
+// console.log(MyTimeStamp);
+// console.log(Math.floor(MyTimeStamp/1000));
+
+// costomize .toLocaleString
+const DateXYZ = new Date()
+// console.log(DateXYZ.toLocaleString('default', {
+//     dateStyle: "long"
+// }));
+
+
+/* <------------------ Dates End ------------------> */
