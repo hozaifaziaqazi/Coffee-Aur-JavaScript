fa96b33f3a944ab9fc088ab7cfd4a7a7c450816a
Hozaifa <hozaifaziaqazi@gmail.com>
2024-05-30 12:50:19 +0500

    added 01_array.js file

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,63 @@
+console.log('Array In JS');
+
+/* ++++++++++++++++++++ Array ++++++++++++++++++++ */
+
+/* <------------- Basic Array Start -------------> */
+
+// const MyArr = [0, 1, 'hozaifa', true, undefined, null];
+// console.log(MyArr[2]);
+
+/* <-------------- Basic Array End --------------> */
+
+
+
+/* <------------- Array Method Start -------------> */
+
+const UserN = [0, 1, 2, 3, 4, 5];
+const UserS = ['hozaifa'];
+const UserB = [true, false];
+const UserU = [undefined];
+const Usern = [null];
+
+// const UserOne = UserN
+// console.log(UserOne.push(5)); // Does not work
+// UserOne.push(6);
+// UserOne.push(7);
+// console.log(UserOne);
+
+// const UserTwo = UserN
+// console.log(UserTwo.pop()); // Does not work
+// UserTwo.pop();
+// console.log(UserTwo);
+
+// const UserThree = UserN
+// UserThree.shift();
+// console.log(UserThree);
+
+// const UserFour = UserN
+// UserFour.unshift(8);
+// console.log(UserFour);
+
+// const UserFive = UserN
+// console.log(UserFive.includes(2));
+// console.log(UserFive.includes(9));
+
+// const UserSix = UserN
+// console.log(UserSix.indexOf(3));
+// console.log(UserSix.indexOf(9));
+
+// const UserSeven = UserN
+// let MyVar = UserN.join();
+// console.log(MyVar);
+// console.log(typeof MyVar);
+// console.log(UserSeven);
+
+// const UserEight = UserN
+// console.log('A', UserEight);
+// console.log('B', UserEight.slice(0, 3));
+
+// const UserNine = UserN
+// console.log('A', UserNine.splice(0, 3));
+// console.log('B', UserNine);
+
+/* <-------------- Array Method End --------------> */
