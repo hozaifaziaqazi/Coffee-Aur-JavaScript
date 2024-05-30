085d95cbea0a30a55ab4280444f4bbb7f65659c9
Hozaifa <hozaifaziaqazi@gmail.com>
2024-05-31 01:04:29 +0500

    added file '02_Array.js' advance array

---
+++ 02_Basics/02_Array.js
@@ -0,0 +1,43 @@
+console.log('Advance Array in JS');
+
+/* +++++++++++++++++ Advance Array +++++++++++++++++ */
+
+/* <------------- Advance Array Start -------------> */
+
+const Marvel_Heros = ['Ironman', 'Captain America', 'Spiderman','Thor']
+const DC_Heros = ['Superman', 'Flash', 'Batman']
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+let score4 = 400;
+let score5 = 500;
+
+// const One = Maervel_Heros
+// UserOne.push(DC_Heros)
+// console.log(One [4][2]);
+// console.log(One);
+
+// const Two = Marvel_Heros.concat(DC_Heros)
+// console.log(Two);
+
+// const Three = [...Marvel_Heros, ...DC_Heros]
+// console.log(Three);
+
+// const Four = [1, 2, 3,[ 4, 5, 6], 7, [8, 9, 10, [11, 12, 13, [14, 15, 16, [17, 18, 19]]]],20]
+// console.log(Four.flat(4));
+// console.log(Four.flat(Infinity));
+
+// const Five = Array.isArray("Hozaifa Zia")
+// console.log(Five);
+
+
+// const Six = Array.from("Hozaifa Zia")
+// let a = Array.from({Name: "Hozaifa Zia"})
+// console.log(x);
+// console.log(Six);
+
+
+// const Seven = Array.of(score1, score2, score3, score4, score5)
+// console.log(Seven);
+
+/* <-------------- Advance Array End --------------> */
