12458256e7d254b0a7883bd2b048d38094033762
Hozaifa <hozaifaziaqazi@gmail.com>
2024-06-11 00:14:58 +0500

    modified 01_Functions.js "Intigreat Func with Obj and Array"

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -2,31 +2,32 @@ console.log('Functions In JS');
 
 /* +++++++++++++++++++ Functions +++++++++++++++++++ */
 
-/* <--------------- Functions Start ---------------> */
 
-function SayMyName() {
-    console.log('H');
-    console.log('O');
-    console.log('Z');
-    console.log('A');
-    console.log('I');
-    console.log('F');
-    console.log('A');
-} 
+/* <------------ Basic Functions Start ------------> */
+
+// function SayMyName() {
+//     console.log('H');
+//     console.log('O');
+//     console.log('Z');
+//     console.log('A');
+//     console.log('I');
+//     console.log('F');
+//     console.log('A');
+// } 
 
 // SayMyName()
 
 //            ⬇     ⬇ these two called parameters 
-function Sum(num1, num2) {
-    console.log(num1 + num2);
-}
+// function Sum(num1, num2) {
+//     console.log(num1 + num2);
+// }
 
-function AddTwoNumbers(num1, num2) {
-    // let adding =  num1 + num2
-    // return adding
+// function AddTwoNumbers(num1, num2) {
+//     // let adding =  num1 + num2
+//     // return adding
 
-    return num1 + num2
-}
+//     return num1 + num2
+// }
 
 // Sum(10, 20)       // ⬅
 // Sum(10, '20')    // ⬅ 
@@ -41,24 +42,65 @@ function AddTwoNumbers(num1, num2) {
 // console.log("Result of Addition: ",resultOfAddition)
 
 
-function UserLogInMessage(username = "hozaifa") {
-    if(username === undefined) {
-        console.log('Please Enter a Username');
-        return
-    }
-    return `${username} just logged in`
-}
+// function UserLogInMessage(username = "hozaifa") {
+//     if(username === undefined) {
+//         console.log('Please Enter a Username');
+//         return
+//     }
+//     return `${username} just logged in`
+// }
 
 // console.log(UserLogInMessage("hozaifa"));
 // console.log(UserLogInMessage("") );
-console.log(UserLogInMessage());
+// console.log(UserLogInMessage());
+
+/* <------------- Basic Functions End -------------> */
 
 
 
 
 
 
+/* <--------- Functions And Objects Start ---------> */
+
+// function Calculate_Cart_Price(...num1){
+//     return num1
+// }
+
+// console.log(Calculate_Cart_Price(200, 400, 600, 800, 1000));
+
+
+// function Cart_Price(val1, val2, val3, ...num1){
+//     return num1
+// }
+
+// console.log(Cart_Price(200, 400, 600, 800, 1000));
+
+// const user = {
+//     username: "hozaifaziaqazi",
+//     prices: 199
+// }
+
+// function Handle_Object(anyObject) {
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+
+// }
+
+// Handle_Object(user)
+// Handle_Object({
+//     username: "Hozaifa Zia",
+//     price: 16789,
+// })
 
 
 
-/* <---------------- Functions End ----------------> */
+const func_Array = [100, 300, 500, 700, 900]
+
+function Hadle_Array(getArray){
+    return getArray[2]
+}
+
+console.log(Hadle_Array(func_Array));
+console.log(Hadle_Array([1, 3, 5, 7, 9]));
+
+/* <--------- Functions And Objects End -----------> */
