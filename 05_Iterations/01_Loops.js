b3e07ddec3e99d524ab70189e670a0a84cdba6cb
Hozaifa <hozaifaziaqazi@gmail.com>
2024-07-18 06:09:32 +0500

    added 01_Loops.js file Topic: For Loop, Break, Continue

---
+++ 05_Iterations/01_Loops.js
@@ -0,0 +1,54 @@
+console.log('Loops In JS');
+
+/* +++++++++++++++++++++ Loops +++++++++++++++++++++ */
+
+/* ++++++++++++++++++ Loops Start ++++++++++++++++++ */
+
+// for (let i = 0; i <= 10; i++) {
+    //     const element = i;
+    //     if (element == 5) {
+        //         console.log('5 is the best number'); 
+        //     }
+        //     console.log(`No ${element}`);
+        
+        // }
+        
+        
+        // for (let i = 0; i  <= 100; i++) {
+            //     console.log(`Check Loop No: ${i}`);
+            //     for (let j = 0; j  <= 10; j++) {
+                //         // console.log(`Inner Loop No: ${j} Check No: ${i}`);
+                //          console.log(`${i} * ${j} = ${i * j}`); 
+                
+                //     }
+    
+// }
+
+
+// MyArr = ["Faisal", "Fahad", "Asad"]
+// for (let u = 0; u < MyArr.length; u++) {
+    //     const element = MyArr[u];
+//     console.log(element);
+// }
+
+
+/* ############### Break and Continue ############### */
+
+// for (let index = 0; index <= 20; index++) {
+    //     if (index == 5) {
+        //         console.log('Detected 5');
+        //         break;
+        //     }
+        //     console.log(`Value of I is ${index}`);    
+        // }
+        
+        
+        for (let index = 0; index <= 20; index++) {
+            if (index == 5) {
+                console.log('Detected 5');
+                continue;
+            }
+            console.log(`Value of I is ${index}`);    
+        }
+        
+/* +++++++++++++++++++ Loops End +++++++++++++++++++ */
